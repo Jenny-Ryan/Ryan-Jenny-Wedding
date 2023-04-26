074bbcf5b446b4b8698fefc82b82ce9b811ab860
Jenny <jianingcao0324@gmail.com>
2023-04-25 23:10:50 -0700

    optional menu drop down

--- contact.js
+++ contact.js
@@ -1,19 +1,5 @@
-// // Get the button
-// let mybutton = document.getElementById("myBtn");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+/* Toggle the show-menu class on the header when the menu button is clicked */
+function toggleMenu() {
+    var header = document.querySelector('.mobile-nav');
+    header.classList.toggle('show-menu');
+  }
